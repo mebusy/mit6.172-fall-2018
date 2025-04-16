ef9af35a19b20cfaa5ab9ed430ecb0ce3369a15c
mebusy <mebusy@mebusys-iMac.ubisoft.org>
2025-04-16 10:55:20 +0800

    * change code for MacOS + opencilk

--- projects/project4/project4/player/util.h
+++ projects/project4/project4/player/util.h
@@ -4,33 +4,37 @@
 #define UTIL_H
 
 #include <inttypes.h>
-#include <unistd.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <time.h>
+#include <unistd.h>
+#ifdef __APPLE__
+#include <sys/sysctl.h>
+#else
 #include <sys/sysinfo.h>
+#endif
 
 #ifndef DEBUG_VERBOSE
-  #define DEBUG_VERBOSE 0
+#define DEBUG_VERBOSE 0
 #endif
 
 #if DEBUG_VERBOSE
-  // DEBUG logging thresh level;
-  // When calling debug_log, only messages with log_level >= DEBUG_LOG_THRESH get printed
-  // so "important" debugging messages should get higher level
-  #define DEBUG_LOG_THRESH 3
-  #define DEBUG_LOG(arg...) debug_log(arg)
-  #define WHEN_DEBUG_VERBOSE(ex) ex
+// DEBUG logging thresh level;
+// When calling debug_log, only messages with log_level >= DEBUG_LOG_THRESH get printed
+// so "important" debugging messages should get higher level
+#define DEBUG_LOG_THRESH 3
+#define DEBUG_LOG(arg...) debug_log(arg)
+#define WHEN_DEBUG_VERBOSE(ex) ex
 #else
-  #define DEBUG_LOG_THRESH 0
-  #define DEBUG_LOG(arg...)
-  #define WHEN_DEBUG_VERBOSE(ex)
+#define DEBUG_LOG_THRESH 0
+#define DEBUG_LOG(arg...)
+#define WHEN_DEBUG_VERBOSE(ex)
 #endif  // EVAL_DEBUG_VERBOSE
 
 #if MACPORT
-  #include "./fasttime.h"
+#include "./fasttime.h"
 #endif
 void debug_log(int log_level, const char* str, ...);
-double  milliseconds();
+double milliseconds();
 uint64_t myrand();
 #endif  // UTIL_H
